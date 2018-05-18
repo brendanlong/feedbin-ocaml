95ff0c4966bc47db0412314ee7bc301e45343e4c
Brendan Long <self@brendanlong.com>
2018-05-18 19:41:55 -0400

    Don't expose of_string functions

--- feedbin/src/feed.mli
+++ feedbin/src/feed.mli
@@ -7,6 +7,4 @@ type t = Feed_t.feed =
   ; site_url : Uri.t }
 [@@deriving compare, sexp_of]
 
-val of_string : string -> (t, [> Parse.error ]) Result.t
-
 val get_by_id : Client.t -> int -> (t option, [> Client.error | Parse.error ]) Lwt_result.t
--- feedbin/src/subscription.mli
+++ feedbin/src/subscription.mli
@@ -18,7 +18,6 @@ type multiple_options =
   [ `Multiple_options of multiple_option list ]
 [@@deriving compare, sexp_of]
 
-val of_string : string -> (t, [> Parse.error ]) Result.t
 val get_by_id : Client.t -> int -> (t option, [> Client.error | Parse.error ]) Lwt_result.t
 val get_all : Client.t -> (t list, [> Client.error | Parse.error ]) Lwt_result.t
 val create_for_url : Client.t -> string -> (t option, [> Client.error | Parse.error | multiple_options ]) Lwt_result.t
